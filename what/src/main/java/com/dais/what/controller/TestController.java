a58b01d9232afe574b0fcaef9f546b677f2070a2
totodle <daiswhat@gmail.com>
2026-03-28 20:40:59 +0900

    fix the CI/CD

--- what/src/main/java/com/dais/what/controller/TestController.java
+++ what/src/main/java/com/dais/what/controller/TestController.java
@@ -8,6 +8,6 @@ public class TestController {
 
     @GetMapping("/api/check")
     public String check() {
-        return "백엔드 서버가 살아있습니다! DB 연결 대기 중....";
+        return "백엔드 서버가 살아있습니다! DB 연결 대기 중.... 수정해볼게요";
     }
 }
